29d58157371f16820dfb20eb83e4cc6d9fa900a6
vencia <vencia.h@gmail.com>
2019-04-08 15:55:32 +0200

    ignore isolated vertices for mesh pool selection

--- scripts/train_coseg.sh
+++ scripts/train_coseg.sh
@@ -4,11 +4,10 @@ PROJECT_ROOT=$(dirname $(realpath "$0"))/..
 
 cd "$PROJECT_ROOT"
 
-PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 0 0 0 0
-PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 0 0 0 10
-PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 10 0 0 0
+#PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 0 0 0 0
+#PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 0 0 0 10
 PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 5 5 5 5
-PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 10 10 10 10
+PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 150 150 150 21
 
 
 
--- torch_geometric/nn/pool/mesh_pool.py
+++ torch_geometric/nn/pool/mesh_pool.py
@@ -1,15 +1,20 @@
 import torch
 
 
+def is_in(ar1, ar2):
+    return (ar1[..., None] == ar2).any(-1)
+
+
 def mesh_pool(data, count, aggr='add'):
+    """
+    MeshCNN-like pooling but with node features instead of edge features.
+    <https://arxiv.org/abs/1809.05910>
+    """
+    isolated_nodes_mask = is_in(torch.arange(data.num_nodes, device=torch.device('cuda')), data.edge_index[0]) != 1
     for _ in range(count):
-        summed_attributes = data.x.sum(dim=-1)
+        summed_attributes = data.x.sum(dim=-1) + isolated_nodes_mask.float() * 100000.0  # some high value
         x_min = summed_attributes.min(dim=0)[1]
         x_neighbors = data.edge_index[1][(data.edge_index[0] == x_min).nonzero()].squeeze(dim=1)
-        if len(x_neighbors) == 0:  # isolated vertex
-            data.x[y_min] = data.x[x_min] + data.x[x_min]  # some high value , TODO: change
-            continue
-        # print('shape', data.shape_id.cpu().item(), 'edge', x_min.cpu().item(), x_neighbors.cpu().numpy())
         y_min = x_neighbors[summed_attributes[x_neighbors].min(dim=0)[1]]
         y_neighbors = data.edge_index[1][(data.edge_index[0] == y_min).nonzero()].squeeze(dim=1)
         both_neighbors = [x for x in x_neighbors if x in y_neighbors]
@@ -18,7 +23,7 @@ def mesh_pool(data, count, aggr='add'):
         #           len(both_neighbors))
 
         # contract edge (x_min, y_min)
-        # new node gets idx of x_min with updated edges, y_min gets dummy value and gets deleted from edges
+        # new node gets idx of x_min with updated edges, y_min gets zeroed and gets deleted from edges
         data.pos[x_min] = (data.pos[x_min] + data.pos[y_min]) / 2
         if aggr == 'max':
             new_node_attr = torch.max(data.x[x_min], data.x[y_min])  # TODO: doesn't work because of inplace operation?
@@ -28,8 +33,8 @@ def mesh_pool(data, count, aggr='add'):
             assert aggr == 'add'
             new_node_attr = data.x[x_min] + data.x[y_min]
         data.x[x_min] = new_node_attr
-        data.x[y_min] = data.x[x_min] + data.x[
-            x_min]  # some high value, TODO: change, because then wrong global pooling etc
+        data.x[y_min] = torch.zeros_like(data.x[y_min])
+        isolated_nodes_mask[y_min] = 1
 
         # remove now duplicate y_min edges
         row, col = data.edge_index
