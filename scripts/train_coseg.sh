e80f791b17956239e199264fc8c536a21e96d94e
vencia <vencia.h@gmail.com>
2019-04-08 20:40:53 +0200

    fix coseg train script, mixup of #faces vs #edges

--- scripts/train_coseg.sh
+++ scripts/train_coseg.sh
@@ -6,8 +6,11 @@ cd "$PROJECT_ROOT"
 
 #PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 0 0 0 0
 #PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 0 0 0 10
-PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 5 5 5 5
-PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 150 150 150 21
+#PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 5 5 5 5
+PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 50 50 50 7
+PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 25 25 25 3
+PYTHONPATH=. python examples/coseg.py --epochs 1000 --pool 12 12 12 2
+
 
 
 
